b795eb633d0e5569adb60198d9f2a9f3295680b3
NewIncome <alfredo_cardenas@live.com>
2019-11-15 15:03:19 -0600

    added my_each method, apparently fully functional.

--- js_methods.rb
+++ js_methods.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 # rubocop:disable Style/LineLength, Style/StringLiterals, Style/CaseEquality
 
+module Enumerable
+  def my_each
+    for e in self
+      yield e
+    end
+  end
+end
+
+{"a":1, "b":2, "c":3, "d":4}.my_each { |e| puts e  }
+
 # rubocop:enable Style/LineLength, Style/StringLiterals, Style/CaseEquality
 # //cop  <-- configuration option
