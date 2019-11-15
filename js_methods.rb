71b77036a91448d4c6084dd8bbcc88d1b1ecf661
NewIncome <alfredo_cardenas@live.com>
2019-11-15 12:21:20 -0600

    Added linter and rubocop files and special commits on code file

---
+++ js_methods.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+# rubocop:disable Style/LineLength, Style/StringLiterals, Style/CaseEquality
+
+# rubocop:enable Style/LineLength, Style/StringLiterals, Style/CaseEquality
+# //cop  <-- configuration option
