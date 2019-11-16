362dcb6ae7e5c95aafb46933de4a5ce58a217c80
NewIncome <alfredo_cardenas@live.com>
2019-11-15 21:26:04 -0600

    Added the my_each_with_index method

--- js_methods.rb
+++ js_methods.rb
@@ -10,14 +10,22 @@ module Enumerable
     end
   end
 
+  def my_each_with_index
+    i = 0
+    for e in self
+      yield e, i if block_given?
+      i += 1
+    end
+  end
+
 end
 
-my_hash = { "a": 1, "b": 2, "c": 3, "d": 4 }
+my_hash = { "a" => 1, "b" => 2, "c": 3, "d": 4 }
 my_array = %w[ a b c d e ]
 # { "a": 1, "b": 2, "c": 3, "d": 4 }.my_each { |k, v| puts "key: #{k}, val: #{v}" }
 # puts "true? #{e}, temp: #{temp}, acc: #{temp_arr}"
-my_array.my_each { |k, v| p "key: #{k}, val: #{v}" }; puts
-my_hash.my_each { |k, v| puts "key: #{k}, val: #{v}" }
+my_hash.each_with_index { |k, v| p "key: #{k}, val: #{v}" }; puts
+my_hash.my_each_with_index { |k, v| p "key: #{k}, val: #{v}" }
 
 # rubocop:enable Style/LineLength, Style/StringLiterals, Style/CaseEquality
 # //cop  <-- configuration option
