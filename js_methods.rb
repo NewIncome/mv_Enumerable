a4922984bbac207b134b08fcdf48f8f9b3bfbf75
NewIncome <alfredo_cardenas@live.com>
2019-11-15 21:31:48 -0600

    Added my_none? method

--- js_methods.rb
+++ js_methods.rb
@@ -43,6 +43,14 @@ module Enumerable
     ans
   end
 
+  def my_none?
+    ans = true
+    self.my_each do |e|
+      ans = false if yield e
+    end
+    ans
+  end
+
 end
 
 my_hash = { "a" => 1, "b" => 2, "c": 3, "d": 4 }
@@ -57,6 +65,8 @@ puts [1, 2, 3, 4, 5].my_select { |n| n % 2 != 0 }
 puts [2, 4, 6].my_all? { |n| n % 2 == 0 }
 
 puts [2, 4, 6].my_any? { |n| n % 2 != 0 }
+
+puts [2, 4, 6].my_none? { |n| n % 2 != 0 }
 
 # rubocop:enable Style/LineLength, Style/StringLiterals, Style/CaseEquality
 # //cop  <-- configuration option
