9ee8e32cc78d24f88ec56bd5a5e3e415e760426c
NewIncome <alfredo_cardenas@live.com>
2019-11-15 21:27:41 -0600

    Added my_select method

--- js_methods.rb
+++ js_methods.rb
@@ -18,6 +18,15 @@ module Enumerable
     end
   end
 
+  def my_select
+    temp_arr = []
+    self.my_each do |e|
+      temp = yield e
+      temp_arr << e if temp
+    end
+    temp_arr
+  end
+
 end
 
 my_hash = { "a" => 1, "b" => 2, "c": 3, "d": 4 }
@@ -26,6 +35,8 @@ my_array = %w[ a b c d e ]
 # puts "true? #{e}, temp: #{temp}, acc: #{temp_arr}"
 my_hash.each_with_index { |k, v| p "key: #{k}, val: #{v}" }; puts
 my_hash.my_each_with_index { |k, v| p "key: #{k}, val: #{v}" }
+
+puts [1, 2, 3, 4, 5].my_select { |n| n % 2 != 0 }
 
 # rubocop:enable Style/LineLength, Style/StringLiterals, Style/CaseEquality
 # //cop  <-- configuration option
