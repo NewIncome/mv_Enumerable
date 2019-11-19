979834b0ffe9ec9d74b947829ec42ed538339f2c
NewIncome <alfredo_cardenas@live.com>
2019-11-19 09:16:00 -0600

    Fixed first linter errors

--- js_methods.rb
+++ js_methods.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
-# rubocop:disable Style/LineLength, Style/StringLiterals, Style/CaseEquality
+# rubocop:disable Style/StringLiterals
 
 module Enumerable
-
   def my_each
     for e in self
       yield e if block_given?
@@ -20,7 +19,7 @@ module Enumerable
 
   def my_select
     temp_arr = []
-    self.my_each do |e|
+    my_each do |e|
       temp = yield e
       temp_arr << e if temp
     end
@@ -29,7 +28,7 @@ module Enumerable
 
   def my_all?
     ans = true
-    self.my_each do |e|
+    my_each do |e|
       ans = false unless yield e
     end
     ans
@@ -37,7 +36,7 @@ module Enumerable
 
   def my_any?
     ans = false
-    self.my_each do |e|
+    my_each do |e|
       ans = true if yield e
     end
     ans
@@ -45,42 +44,44 @@ module Enumerable
 
   def my_none?
     ans = true
-    self.my_each do |e|
+    my_each do |e|
       ans = false if yield e
     end
     ans
   end
 
-  def my_count(*chr) # why can't use *char ???
-    cnt = 0; char = *chr
-    unless char.empty?
-      self.my_each { |e| cnt += 1 if e == char[0] }
+  def my_count(*chr)
+    # why can't use *char directly???
+    cnt = 0
+    char = *chr
+    if !char.empty?
+      my_each { |e| cnt += 1 if e == char[0] }
       cnt
     else
-      self.length
+      length
     end
   end
 
   def my_map
     arr = []
     if block_given?
-      self.my_each do |e|
+      my_each do |e|
         elm = yield e
         arr << elm
       end
       arr
     else
-      self.to_enum
+      to_enum
     end
   end
 
   def my_inject(*init)
     init, sym = *init
-    acc = init if init.is_a?(Fixnum)
+    acc = init if init.is_a?(Integer)
     if !sym.nil? || init.is_a?(Symbol)
       sym = init if sym.nil? # to exchange values in case of 1 param.
-      self.my_each do |e|
-        acc = e and next if acc.nil? # to not sum 2times 1st value
+      my_each do |e|
+        acc = e && next if acc.nil? # to not sum 2times 1st value
         acc += e if sym == :+
         acc -= e if sym == :-
         acc *= e if sym == :*
@@ -88,8 +89,8 @@ module Enumerable
       end
       acc
     elsif block_given?
-      self.my_each do |e|
-        acc = e and next if acc.nil? # to not sum 2times 1st value
+      my_each do |e|
+        acc = e && next if acc.nil? # to not sum 2times 1st value
         acc = yield acc, e unless acc.nil?
       end
     end
@@ -98,12 +99,11 @@ module Enumerable
 
   def my_map_bproc(&bproc)
     arr = []
-    self.my_each do |e|
+    my_each do |e|
       arr << bproc.call(e)
     end
     arr
   end
-
 end
 
 def self.multiply_els(arr)
@@ -111,11 +111,12 @@ def self.multiply_els(arr)
 end
 
 my_hash = { "a" => 1, "b" => 2, "c": 3, "d": 4 }
-my_l_array = %w[ a b c d e ]
+my_lt_array = %w[a b c d e]
 my_n_array = [1, 2, 3, 4]
 # { "a": 1, "b": 2, "c": 3, "d": 4 }.my_each { |k, v| puts "key: #{k}, val: #{v}" }
 # puts "true? #{e}, temp: #{temp}, acc: #{temp_arr}"
-my_hash.each_with_index { |k, v| p "key: #{k}, val: #{v}" }; puts
+my_hash.each_with_index { |k, v| p "key: #{k}, val: #{v}" }
+puts
 my_hash.my_each_with_index { |k, v| p "key: #{k}, val: #{v}" }
 
 puts [1, 2, 3, 4, 5].my_select { |n| n % 2 != 0 }
@@ -145,12 +146,13 @@ p multiply_els([2,4,5])
 puts
 bproc = Proc.new { |e| p "Test: #{e}" }
 [1, 2, ""].my_map_bproc(&bproc)
-[1, 2, ""].map { |e| p "Test: #{e}" }
+[1, 2, ""].my_map_bproc { |e| p "Test: #{e}" }
 [1, 2, ""].my_map(&bproc)
+[1, 2, ""].my_map { |e| p "Test: #{e}" }
 [1, 2, ""].map(&bproc)
 [1, 2, ""].map { |e| p "Test: #{e}" }
 # .send   , checkITout
 
 #p my_n_array.my_inject
-# rubocop:enable Style/LineLength, Style/StringLiterals, Style/CaseEquality
+# rubocop:enable Style/StringLiterals
 # //cop  <-- configuration option
